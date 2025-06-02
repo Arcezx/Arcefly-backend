ed8c497153952717cf722da5e170cc688124a5ca
Cristian <Arcezx>
2025-06-02 20:27:02 +0200

    Actualizado datos para app (funciona)

--- src/main/java/com/example/prueba/config/AppConfig.java
+++ src/main/java/com/example/prueba/config/AppConfig.java
@@ -6,8 +6,5 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class AppConfig {
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+
 }
