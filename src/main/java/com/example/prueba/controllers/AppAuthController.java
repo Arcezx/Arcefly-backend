f7ab0286e1313f0e4d3b3b38cea2fe2d8a87e506
Cristian <Arcezx>
2025-06-03 11:48:37 +0200

    Actualizado datos para app 030625

--- src/main/java/com/example/prueba/controllers/AppAuthController.java
+++ src/main/java/com/example/prueba/controllers/AppAuthController.java
@@ -1,26 +1,4 @@
 package com.example.prueba.controllers;
 
-
-import com.example.prueba.dtos.requests.AppLoginRequest;
-import com.example.prueba.dtos.responses.AppLoginResponse;
-import com.example.prueba.services.AppAuthService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/app/auth")
 public class AppAuthController {
-
-    @Autowired
-    private AppAuthService appAuthService;
-
-    @PostMapping("/login")
-    public ResponseEntity<AppLoginResponse> login(@RequestBody AppLoginRequest request) {
-        AppLoginResponse response = appAuthService.authenticate(request);
-        return ResponseEntity.ok(response);
-    }
 }
---
+++ src/main/java/com/example/prueba/dtos/requests/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.example.prueba.dtos.requests;
+
+public class LoginRequest {
+}
